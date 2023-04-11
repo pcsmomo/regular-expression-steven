38ff149d59050a17df497f45e06c4e8a62be2cb0
Noah Kim <dongwoo_333@hanmail.net>
2023-04-11 22:37:46 +1000

    3. Getting Started

--- 01-getting-started/03-intro.js
+++ 01-getting-started/03-intro.js
@@ -1,1 +1,7 @@
-let txt = "Programming courses always starts with a hello world example.";
+const txt = "Programming courses always starts with a hello world example.";
+
+const regex1 = new RegExp("hello");
+const regex2 = /world/;
+
+console.log(regex1.test(txt)); // true
+console.log(regex2.test(txt)); // true
